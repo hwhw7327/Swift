270b61e1979f76a991b6aa77fe54134c2b1592c8
hwo1013@naver.com <hwo1013@naver.com>
2023-05-07 20:46:35 +0900

    Basic

---
+++ Basic.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+//var greeting = "Hello, playground"
+
+//var str = "hello swift"
+//str = "hello world"
+//
+//var num = 3
+
+var age = 20
+
+//age = "Hi" -> Int타입으로 이미 지정되어서 Stirng 타입 넣을 수 없음
+
+//큰 숫자는 "_" 를 써서 읽기 쉽게 숫자를 표현 할 수 있음
+var money = 2_000_000
+var price = 1000200
+
+var name = "Jason"
+name = "Jin"
+
+
+var gpa = 4.5
+var isEnabled = false
+
+// String Interpolation
+var height = 180
+var text = "his height is \(height) "
+
+// 상수 할당
+let dj = "koo"
+
+// dj = "goo" -> 시도하면 에러발생, 상수는 할당된 값을 변경할 수 없기때문.
+
+//명시적 타입 기입 없을 떄 -> 타입 추론
+var weeks = 10
+var token = "qwer123"
+
+//명시적 타입 기입
+var days : Int = 5
+var userName: String = "Jake"
